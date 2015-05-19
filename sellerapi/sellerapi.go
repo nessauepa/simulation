007919b8f4f66ee10133c58fc89e98a05a9526b5
Vanessa Schissato <vanessaschissato@gmail.com>
2015-05-18 23:26:02 -0300

    Added commentaries + Go fmt

--- sellerapi/sellerapi.go
+++ sellerapi/sellerapi.go
@@ -1,5 +1,9 @@
+/*
+Package sellerapi implements a client to the Seller API.
+*/
 package sellerapi
 
+// Simulate make a order simulation
 func Simulate() string {
 	
 	return "Seller API fake return";
--- simulation.go
+++ simulation.go
@@ -7,6 +7,6 @@ import (
 
 func main() {
 
-	fmt.Println(sellerapi.Simulate());
+	fmt.Println(sellerapi.Simulate())
 
 }
